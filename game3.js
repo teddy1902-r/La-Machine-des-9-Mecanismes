10264762d38de9b97b4589612e6f3bb83e791b0b
teddy1902-r <teddysegura.ts@gmail.com>
2026-09-14 23:37:26 +0200

    Ajoute le mécanisme 3

---
+++ game3.js
@@ -0,0 +1,4 @@
+games[3]=function(){
+  makeRouteGame({id:3,size:5,kind:'pipe',leftIcon:'♨️',rightIcon:'🌀',leftLabel:'CHAUDIÈRE',rightLabel:'TURBINE',
+    route:[[2,0],[1,0],[1,1],[2,1],[2,2],[3,2],[3,3],[2,3],[2,4]]});
+};
