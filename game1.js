00a76ec30ed0f715d80231c5f3c34795a6719a07
teddy1902-r <teddysegura.ts@gmail.com>
2026-09-14 23:37:12 +0200

    Ajoute le mécanisme 1

---
+++ game1.js
@@ -0,0 +1,4 @@
+games[1]=function(){
+  makeRouteGame({id:1,size:7,kind:'wire',leftIcon:'⚡',rightIcon:'💠',leftLabel:'GÉNÉRATEUR',rightLabel:'CŒUR',
+    route:[[3,0],[3,1],[2,1],[2,2],[1,2],[1,3],[1,4],[2,4],[2,5],[3,5],[3,6]]});
+};
