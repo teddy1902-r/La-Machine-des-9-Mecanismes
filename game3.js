39978fe0b2c5035aaf8b241d2d0ea88c116d6aa8
teddy1902-r <teddysegura.ts@gmail.com>
2026-09-15 00:04:08 +0200

    Corriger les bugs et renforcer les 9 mécanismes

--- game3.js
+++ game3.js
@@ -1,4 +1,4 @@
 games[3]=function(){
-  makeRouteGame({id:3,size:5,kind:'pipe',leftIcon:'♨️',rightIcon:'🌀',leftLabel:'CHAUDIÈRE',rightLabel:'TURBINE',
-    route:[[2,0],[1,0],[1,1],[2,1],[2,2],[3,2],[3,3],[2,3],[2,4]]});
+  makeRouteGame({id:3,size:7,kind:'pipe',leftIcon:'♨️',rightIcon:'🌀',leftLabel:'CHAUDIÈRE',rightLabel:'TURBINE',
+    route:[[3,0],[3,1],[2,1],[2,2],[1,2],[1,3],[1,4],[2,4],[2,5],[3,5],[4,5],[4,6],[3,6]]});
 };
