eea92ffde3babc663a5a457b3732d585320f1144
teddy1902-r <teddysegura.ts@gmail.com>
2026-09-15 19:03:28 +0200

    Ajouter des fausses pistes au circuit d energie

--- game1.js
+++ game1.js
@@ -1,4 +1,5 @@
 games[1]=function(){
   makeRouteGame({id:1,size:9,kind:'wire',leftIcon:'⚡',rightIcon:'💠',leftLabel:'GÉNÉRATEUR',rightLabel:'CŒUR',
+    fillDecoys:true,
     route:[[4,0],[4,1],[3,1],[3,2],[2,2],[2,3],[1,3],[1,4],[2,4],[2,5],[3,5],[3,6],[4,6],[4,7],[5,7],[5,8],[4,8]]});
 };
